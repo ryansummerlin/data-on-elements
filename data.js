3ca70a95d3ba99234df45072e82accbf4be1a975
ryansummerlin <ryansummerlin777@gmail.com>
2023-04-23 19:21:04 -0700

    Created functionality for the add item button

--- data.js
+++ data.js
@@ -1,1 +1,13 @@
-// Your code here
+window.addEventListener("DOMContentLoaded", event => {
+    const addButton = document.getElementById('add');
+    const shoppinglist = document.getElementById("shopping-list");
+    addButton.addEventListener("click", event => {
+        const name = document.getElementById("name");
+        const type = document.getElementById("type");
+        const newItem = document.createElement("li");
+        newItem.setAttribute("data-type", type.value);
+        newItem.innerText = name.value;
+        shoppinglist.appendChild(newItem);
+        event.preventDefault();
+    });
+});
